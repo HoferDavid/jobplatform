4328dc80fe0767e496985a815f32ad72826f44c1
David Hofer <chassis.poppets0x@icloud.com>
2024-10-27 11:50:07 +0100

    feat: add JobController

--- routes/web.php
+++ routes/web.php
@@ -1,87 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
 
-// Index, All Jobs
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest( )->simplePaginate(3);
-    
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-
-// Create Job
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-
-// Show Job
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
-});
-
-
-// Store Job
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-
-// Edit Job
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-
-// Update Job
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-});
-
-
-// Delete Job
-Route::delete('/jobs/{id}', function ($id) {
-    Job::findOrFail($id)->delete();
-
-    return redirect('/jobs');
-});
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
