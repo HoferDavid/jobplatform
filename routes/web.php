57c632a6d0cf09985aea4b78de138fbda5d4b806
David Hofer <chassis.poppets0x@icloud.com>
2024-10-27 14:25:44 +0100

    feat: register form start

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
 
 Route::view('/', 'home');
@@ -9,3 +11,11 @@ Route::view('/contact', 'contact');
 
 
 Route::resource('jobs', JobController::class);
+
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
